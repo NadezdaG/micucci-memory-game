695b37d1b0695330974348ebdbd412ad739efee9
NadezdaG <nadezda.gurska@gmail.com>
2025-05-12 19:51:16 +0100

    some amends bijoure

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -1,18 +1,5 @@
 <template>
 	<footer>
-		<div class="container">
-			App developed by
-			<a
-				href="https://gurska.dev"
-				title="Nadezda Gurska web developer"
-				target="_blank"
-				>Nadezda G</a
-			>
-			for
-			<a href="https://www.micucci.co.uk" title="" target="_blank"
-				>Micucci</a
-			>.
-		</div>
 	</footer> 
 </template>
 <style lang="scss">
